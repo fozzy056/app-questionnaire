56e7e4f6c767b7341662ad6a42c3bad39c7718f3
fozzy056 <37544663+fozzy056@users.noreply.github.com>
2019-11-05 15:24:41 +0100

    Ajout de la vue Questionnaire + vue Login et implémentation

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s2 offset-s4">
+          <input id="first_name" type="text" class="validate">
+          <label for="first_name">Prénom</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Nom</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s4 offset-s4">
+          <input id="society_name" type="text" class="validate">
+          <label for="society_name">Nom de votre société</label>
+        </div>
+      </div>
+    </form>
+    <a class="waves-effect waves-light btn">Commencer !</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Questionnaire.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="questionnaire">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloQuestionnaire msg="Questionnaire"/>
+    <Login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloQuestionnaire from '@/components/HelloQuestionnaire.vue'
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'questionnaire',
+  components: {
+    HelloQuestionnaire,
+    Login
+  }
+}
+</script>
